85ccd6e404f228ce113b3212ab8208f8821a26f2
Michael Klemersson <mklemersson.dev@gmail.com>
2019-02-15 15:38:42 +0100

    added data faker

---
+++ src/WeatherDataFaker.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace WbApp;
+
+use DateTime;
+
+class WeatherDataFaker
+{
+    const OBSERVATORIES = [
+        'AU' => [
+            'unit' => self::TEMPERATURE_UNIT_CELSIUS,
+            'distance' => self::DISTANCE_TYPE_KILOMETERS
+        ],
+        'US' => [
+            'unit' => self::TEMPERATURE_UNIT_FAHRENHEIT,
+            'distance' => self::DISTANCE_TYPE_MILES
+        ],
+        'FR' => [
+            'unit' => self::TEMPERATURE_UNIT_KELVIN,
+            'distance' => self::DISTANCE_TYPE_METERS
+        ],
+        'Others' => [
+            'unit' => self::TEMPERATURE_UNIT_KELVIN,
+            'distance' => self::DISTANCE_TYPE_KILOMETERS
+        ],
+    ];
+
+    const TEMPERATURE_UNIT_CELSIUS = 'celsius';
+    const TEMPERATURE_UNIT_FAHRENHEIT = 'fahrenheit';
+    const TEMPERATURE_UNIT_KELVIN = 'kelvin';
+    
+    const DISTANCE_TYPE_KILOMETERS = 'km';
+    const DISTANCE_TYPE_MILES = 'km';
+    const DISTANCE_TYPE_METERS = 'km';
+
+    const DATE_FORMAT = 'Y-m-d\TH:i';
+    const DATA_SEPARATOR = '|';
+
+    public function generate(): string
+    {
+        $date = $this->getRandomDate()->format(self::DATE_FORMAT);
+        $observatory = $this->getRandomObservatory();
+        $location = implode(',', $this->getRandomLocation());
+        $temperature = random_int(-2000, 2000);
+
+        return $date . self::DATA_SEPARATOR
+            . $location . self::DATA_SEPARATOR
+            . $temperature . self::DATA_SEPARATOR
+            . $observatory;
+    }
+
+    private function getRandomDate(): DateTime
+    {
+        $date = new DateTime();
+        $year = random_int(1, 3);
+        $month = random_int(1, 12);
+        $day = random_int(1, $month !== 2 ? 30 : 28);
+
+        $date->modify("-{$year} years");
+        $date->modify("-{$month} months");
+        $date->modify("-{$day} days");
+
+        return $date;
+    }
+
+    private function getRandomLocation(): array
+    {
+        return [
+            random_int(0, 20),
+            random_int(0, 20),
+        ];
+    }
+
+    private function getRandomObservatory(): string
+    {
+        $keys = array_keys(self::OBSERVATORIES);
+        $randomIndex = random_int(0, count($keys) - 1);
+
+        return $keys[$randomIndex];
+    }
+}
--- test/Unit/WeatherDataFakerTest.php
+++ test/Unit/WeatherDataFakerTest.php
@@ -2,7 +2,32 @@
 
 namespace Test\WbApp\Unit;
 
-class WeatherDataFaker extends TestCase
-{
+use PHPUnit\Framework\TestCase;
+use WbApp\WeatherDataFaker;
 
+class WeatherDataFakerTest extends TestCase
+{
+    public function testGenerateFakeData()
+    {
+        // prepare
+        $expectedSize = 4;
+        $classUnderTest = new WeatherDataFaker();
+
+        // test
+        $result = $classUnderTest->generate();
+
+        $data = explode(WeatherDataFaker::DATA_SEPARATOR, $result);
+        $dateTime = new \DateTime($data[0]);
+        $location = explode(',', $data[1]);
+        $temperature = $data[2];
+        $observatory = $data[3];
+
+        // assert
+        $this->assertCount($expectedSize, $data);
+        $this->assertCount(2, $location);
+        $this->assertTrue(intval($location[0]) >= 0);
+        $this->assertTrue(intval($location[1]) >= 0);
+        $this->assertIsNumeric($temperature);
+        $this->assertArrayHasKey($observatory, WeatherDataFaker::OBSERVATORIES);
+    }
 }
