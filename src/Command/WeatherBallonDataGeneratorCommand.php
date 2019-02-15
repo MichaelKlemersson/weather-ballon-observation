82853048be93e07120f6538e3db27fb2d205de16
Michael Klemersson <mklemersson.dev@gmail.com>
2019-02-15 15:39:02 +0100

    updated tests

--- src/Command/WeatherBallonDataGeneratorCommand.php
+++ src/Command/WeatherBallonDataGeneratorCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
+use WbApp\WeatherDataFaker;
 
 class WeatherBallonDataGeneratorCommand extends Command
 {
@@ -31,18 +32,25 @@ class WeatherBallonDataGeneratorCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): void
     {
-        $this->createEmptyFile($this->filePath);
+        $this->log("Creating file");
 
-        $linesToGenerate = intval($input->getArgument('lines'));
+        $linesToGenerate = intval($input->getFirstArgument());
+
+        $file = fopen($this->filePath, 'w+');
+
+        $this->log("Writing data...");
+
+        for ($i = 0; $i < $linesToGenerate; $i++) {
+            fwrite($file, $this->weatherDataFaker->generate() . PHP_EOL);
+        }
+
+        fclose($file);
+
+        $this->log("Finished");
     }
 
-    private function createEmptyFile(): void
+    private function log($message)
     {
-
-    }
-
-    private function getRandomData()
-    {
-
+        echo PHP_EOL . $message . PHP_EOL;
     }
 }
