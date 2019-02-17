8d028d884fb92d13651d98cab7355a77fae2fbc9
Michael Klemersson <mklemersson.dev@gmail.com>
2019-02-17 19:15:55 +0100

    added helper metrics

---
+++ src/Util/System.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WbApp\Util;
+
+trait System
+{
+    public function getMemoryPeak(int $precision = 2) {
+        $bytes = memory_get_peak_usage(true);
+
+        $units = array("b", "kb", "mb", "gb", "tb");
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, $precision) . " " . $units[$pow];
+    }
+}
